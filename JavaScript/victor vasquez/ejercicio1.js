066b9aae657da3beb9f6e9427376ce1ac1d81f3c
victorandrees <avasquez142005@gmail.com>
2024-02-26 12:44:10 -0500

    victor js

---
+++ JavaScript/victor vasquez/ejercicio1.js
@@ -0,0 +1,7 @@
+let num = [2,5,3,9];
+let total = num.length;
+
+console.log(num [total-1]);
+
+
+
---
+++ JavaScript/victor vasquez/ejercicio4.js
@@ -0,0 +1,6 @@
+let ds=["domingo","lunes","martes","miercoles","jueves","virnes","sabado"];
+for(let i=0; i<ds.length; i++)
+{
+    ds[i]=ds[i].toUpperCase();
+    console.log(ds);
+}
